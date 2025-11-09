52572e40d2f86e17d320eb8e5ed899076b762cba
Siddharth Shreekumar <shreekumar6061@gmail.com>
2025-11-09 10:22:19 +0530

    Update Day 6_IDC.sql

--- Codes_daywise/Day 6_IDC.sql
+++ Codes_daywise/Day 6_IDC.sql
@@ -8,12 +8,12 @@ USE hospital;
 
 SELECT
 service,
-SUM(patients_admitted) AS 'Total_patients_admitted',
-SUM(patients_refused) AS 'Total_patients_refused',
-ROUND((SUM(patients_admitted) / SUM(patients_request))*100,2) AS 'Admission_rate'
+SUM(`patients_admitted`) AS 'Total_patients_admitted',
+SUM(`patients_refused`) AS 'Total_patients_refused',
+ROUND((SUM(`patients_admitted`) / SUM(`patients_request`))*100,2) AS 'Admission_rate'
 FROM
 services_weekly
-GROUP BY service
+GROUP BY `service`
 ORDER BY Admission_rate DESC;
 
 -- ----------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -21,7 +21,7 @@ ORDER BY Admission_rate DESC;
 -- BONUS Practice
 -- 1. Count the number of patients by each service.
 SELECT
-`service`,COUNT(patient_id) AS 'Count_of_patients'
+`service`,COUNT(`patient_id`) AS 'Count_of_patients'
 FROM
 patients
 GROUP BY `service`;
@@ -29,14 +29,14 @@ GROUP BY `service`;
 -- 2. Calculate the average age of patients grouped by service.
 SELECT
 `service`,
-ROUND(AVG(age),2) AS 'avg_age_of_patients'
+ROUND(AVG(`age`),2) AS 'avg_age_of_patients'
 FROM
 patients
 GROUP BY `service`;
 
 -- 3. Find the total number of staff members per role.
 SELECT
-`role`, COUNT(staff_id) AS 'count_of_staff'
+`role`, COUNT(`staff_id`) AS 'count_of_staff'
 FROM
 staff
 GROUP BY `role`;
