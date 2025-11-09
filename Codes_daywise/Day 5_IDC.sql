159e4498583ba1566f6d4c116fd960cc8ac3ba8d
Siddharth Shreekumar <shreekumar6061@gmail.com>
2025-11-09 10:25:37 +0530

    Update Day 5_IDC.sql

--- Codes_daywise/Day 5_IDC.sql
+++ Codes_daywise/Day 5_IDC.sql
@@ -7,9 +7,9 @@ Round the average satisfaction to 2 decimal places.
 USE hospital;
 
 SELECT
-SUM(patients_admitted) AS 'total_number_of_patients_admitted',
-SUM(patients_refused)AS 'total_patients_refused',
-ROUND(AVG(patient_satisfaction),2) AS 'the average patient satisfaction'
+SUM(`patients_admitted`) AS 'total_number_of_patients_admitted',
+SUM(`patients_refused`)AS 'total_patients_refused',
+ROUND(AVG(`patient_satisfaction`),2) AS 'the average patient satisfaction'
 FROM
 services_weekly;
 
@@ -18,19 +18,19 @@ services_weekly;
 -- BONUS Practice
 -- 1. Count the total number of patients in the hospital.
 SELECT
-COUNT(patient_id) AS 'Total_number_of_patients'
+COUNT(`patient_id`) AS 'Total_number_of_patients'
 FROM
 patients;
 
 -- 2. Calculate the average satisfaction score of all patients.
 SELECT
-ROUND(AVG(satisfaction),2) AS 'average_satisfaction_score'
+ROUND(AVG(`satisfaction`),2) AS 'average_satisfaction_score'
 FROM
 patients;
 
 -- 3. Find the minimum and maximum age of patients.
 SELECT
-MIN(age) AS 'min_age', MAX(age) AS 'max_age'
+MIN(`age`) AS 'min_age', MAX(`age`) AS 'max_age'
 FROM
 patients;
 
